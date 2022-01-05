c8712702b52a43917fe1a0b8f869fc1183ff245e
Kichan <cristian.elias@mercadolibre.com>
2022-01-04 23:43:06 -0300

    Refactoring CSS: Now using BEM for Header component.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,30 +12,33 @@ import "./Header.css";
 const Header = () => {
   const [darkTheme, setDarkTheme] = useState(false);
   return (
-    <header id="header">
-      <nav>
-        <ul>
-          <li className="logo-container">
-            <img id="logo" src={logo} alt="Logo, click to go to homepage" />
-            <div className="logo-background"></div>
-          </li>
-          <li className="theme-switch-container">
-            <img
-              src={darkTheme === true ? lightThemeImage : darkThemeImage}
-              alt="Theme color switch."
-              onClick={() => {
-                setDarkTheme(!darkTheme);
-              }}
-            />
-          </li>
-          <li className="avatar-container">
-            <img
-              id="avatar"
-              src={avatarImage}
-              alt="User avatar, click to go to your profile."
-            />
-          </li>
-        </ul>
+    <header>
+      <nav className="header">
+        <div className="header__logo-container">
+          <img
+            className="header__logo"
+            src={logo}
+            alt="Logo, click to go to homepage"
+          />
+          <div className="header__logo-background"></div>
+        </div>
+        <div className="header__theme-container">
+          <img
+            className="header__theme"
+            src={darkTheme === true ? lightThemeImage : darkThemeImage}
+            alt="Theme color switch."
+            onClick={() => {
+              setDarkTheme(!darkTheme);
+            }}
+          />
+        </div>
+        <div className="header__avatar-container">
+          <img
+            className="header__avatar"
+            src={avatarImage}
+            alt="User avatar, click to go to your profile."
+          />
+        </div>
       </nav>
     </header>
   );

619c8ed30c4fdf17d9fc512d14cf62c6802b8d89
Cris Elias <cristian.elias@mercadolibre.com>
2022-01-22 03:50:52 -0300

    Now properly loading SVGs

--- src/components/GoBack/GoBack.js
+++ src/components/GoBack/GoBack.js
@@ -5,13 +5,27 @@ import { useNavigate } from "react-router-dom";
 // Styles
 import "./GoBack.css";
 
+// Assets
+import iconArrowLeft from "../../assets/icon-arrow-left.svg";
+
 const GoBack = () => {
   const navigate = useNavigate();
 
   return (
-    <button className="go-back" onClick={() => navigate(-1, { replace: true })}>
-      Go back
-    </button>
+    <nav className="go-back">
+      <img
+        className="invoice__arrow"
+        src={iconArrowLeft}
+        alt="Arrow icon. Click to go back."
+      ></img>
+
+      <button
+        className="go-back__button"
+        onClick={() => navigate(-1, { replace: true })}
+      >
+        Go back
+      </button>
+    </nav>
   );
 };
 

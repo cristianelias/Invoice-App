213e25cd195fb6dc24ce9b837ec6c7ee60c65cfb
Cris Elias <cristian.elias@mercadolibre.com>
2022-01-22 23:41:09 -0300

    Added Field component

--- src/components/InvoiceForm/InvoiceForm.js
+++ src/components/InvoiceForm/InvoiceForm.js
@@ -1,4 +1,5 @@
 // Components
+import Field from "./Field/Field";
 import Gradient from "../Gradient/Gradient";
 
 // Styles
@@ -15,268 +16,174 @@ const InvoiceForm = (props) => {
       <form className="invoice-form__form" action="">
         {title}
         <h2 className="h2">Bill From</h2>
-        {/* TODO: COMPONENT  */}
-        <div className="field field--error">
-          <label className="field__label" htmlFor="fromStreetAddress">
-            Street Address
-            <span className="field__validation">
-              This field is required, troesma.
-            </span>
-          </label>
-          <input
-            className="field__input"
-            type="text"
-            name="fromStreetAddress"
-          />
-        </div>
 
         <section className="three-input-container">
-          <div className="field ">
-            <label className="field__label" htmlFor="fromCity">
-              City
-              <span className="field__validation">
-                This field is required, troesma.
-              </span>
-            </label>
-            <input className="field__input" type="text" name="fromCity" />
-          </div>
+          <Field
+            fieldName="fromStreetAddress"
+            labelText="Street Address"
+            validationText="This field is required, troesma."
+            inputType="text"
+          />
 
-          <div className="field ">
-            <label className="field__label" htmlFor="fromPostCode">
-              Post Code
-              <span className="field__validation">
-                This field is required, troesma.
-              </span>
-            </label>
-            <input className="field__input" type="text" name="fromPostCode" />
-          </div>
+          <Field
+            fieldName="fromCity"
+            labelText="City"
+            validationText="This field is required, troesma."
+            inputType="text"
+          />
 
-          <div className="field ">
-            <label className="field__label" htmlFor="fromCountry">
-              Country
-              <span className="field__validation">
-                This field is required, troesma.
-              </span>
-            </label>
-            <input className="field__input" type="text" name="fromCountry" />
-          </div>
+          <Field
+            fieldName="fromPostCode"
+            labelText="Post Code"
+            validationText="This field is required, troesma."
+            inputType="text"
+          />
+
+          <Field
+            fieldName="fromCountry"
+            labelText="Country"
+            validationText="This field is required, troesma."
+            inputType=""
+          />
         </section>
 
         <section>
           <h2 className="h2">Bill To</h2>
 
-          <div className="field ">
-            <label className="field__label " htmlFor="ClientName">
-              Client&apos;s Name
-              <span className="field__validation">
-                This field is required, troesma.
-              </span>
-            </label>
-            <input
-              className="field__input field__input--focus"
-              type="text"
-              name="ClientName"
-            />
-          </div>
+          <Field
+            fieldName="clientName"
+            labelText="Client's Name"
+            validationText="This field is required, troesma."
+            inputType="text"
+          />
 
-          <div className="field ">
-            <label className="field__label" htmlFor="ClientEmail">
-              Client&apos;s Email
-              <span className="field__validation">
-                This field is required, troesma.
-              </span>
-            </label>
-            <input
-              className="field__input"
-              type="Email"
-              name="ClientEmail"
-              placeholder="e.g. email@example.com"
-            />
-          </div>
+          <Field
+            fieldName="clientEmail"
+            labelText="Client's Email"
+            validationText="This field is required, troesma."
+            inputType="email"
+          />
 
-          <div className="field ">
-            <label className="field__label" htmlFor="StreetAddress">
-              Street Address
-              <span className="field__validation">
-                This field is required, troesma.
-              </span>
-            </label>
-            <input className="field__input" type="text" name="StreetAddres" />
-          </div>
+          <Field
+            fieldName="toStreetAddress"
+            labelText="Street Address"
+            validationText="This field is required, troesma."
+            inputType="text"
+          />
 
           <section className="three-input-container">
-            <div className="field ">
-              <label className="field__label" htmlFor="City">
-                City
-                <span className="field__validation">
-                  This field is required, troesma.
-                </span>
-              </label>
-              <input className="field__input" type="text" name="City" />
-            </div>
+            <Field
+              fieldName="toCity"
+              labelText="City"
+              validationText="This field is required, troesma."
+              inputType="text"
+            />
 
-            <div className="field ">
-              <label className="field__label" htmlFor="PostCode">
-                Post Code
-                <span className="field__validation">
-                  This field is required, troesma.
-                </span>
-              </label>
-              <input className="field__input" type="text" name="PostCode" />
-            </div>
+            <Field
+              fieldName="toPostCode"
+              labelText="Post Code"
+              validationText="This field is required, troesma."
+              inputType="text"
+            />
 
-            <div className="field ">
-              <label className="field__label" htmlFor="Country">
-                Country
-                <span className="field__validation">
-                  This field is required, troesma.
-                </span>
-              </label>
-              <input className="field__input" type="text" name="Country" />
-            </div>
+            <Field
+              fieldName="toCountry"
+              labelText="Country"
+              validationText="This field is required, troesma."
+              inputType="text"
+            />
           </section>
         </section>
 
         <section className="two-input-container">
-          <div className="field ">
-            <label className="field__label" htmlFor="InvoiceDate">
-              Invoice Date
-              <span className="field__validation">
-                This field is required, troesma.
-              </span>
-            </label>
-            <input
-              className="field__input"
-              type="date"
-              name="InvoiceDate"
-              placeholder="21 Aug 2021"
-            />
-          </div>
+          <Field
+            fieldName="invoiceDate"
+            labelText="Invoice Date"
+            validationText="This field is required, troesma."
+            inputType="date"
+            placeholder="21 Aug 2021"
+          />
 
-          <div className="field ">
-            <label className="field__label" htmlFor="PaymentTerms">
-              Payment Terms
-              <span className="field__validation">
-                This field is required, troesma.
-              </span>
-            </label>
-            <input
-              className="field__input"
-              type="text"
-              name="PaymentTerms"
-              placeholder=""
-            />
-          </div>
-        </section>
+          <Field
+            fieldName="paymentTerms"
+            labelText="Payment Terms"
+            validationText="This field is required, troesma."
+            inputType="text"
+          />
 
-        <div className="field ">
-          <label className="field__label" htmlFor="ProjectDescription">
-            Project Description
-            <span className="field__validation">
-              This field is required, troesma.
-            </span>
-          </label>
-          <input
-            className="field__input field__input--no-margin-bottom"
-            type="text"
-            name="ProjectDescription"
+          <Field
+            fieldName="projectDescription"
+            labelText="Project Description"
+            validationText="This field is required, troesma."
+            inputType="text"
             placeholder="e.g. Graphic Design Service"
           />
-        </div>
+        </section>
 
         <section>
           <h1 className="item-list">Item List</h1>
 
           <section className="five-fields-container">
-            <div className="field ">
-              <label className="field__label" htmlFor="ItemName">
-                Item Name
-                <span className="field__validation">
-                  This field is required, troesma.
-                </span>
-              </label>
-              <input className="field__input" type="text" name="ItemName" />
-            </div>
+            <Field
+              fieldName="itemName"
+              labelText="Item Name"
+              validationText="This field is required, troesma."
+              inputType="text"
+              classes="five-fields-container__column-item-name"
+            />
 
-            <div className="field ">
-              <label className="field__label" htmlFor="Qty">
-                Qty.
-                <span className="field__validation">
-                  This field is required, troesma.
-                </span>
-              </label>
-              <input
-                className="field__input field__imput--type-number field__input--center"
-                type="number"
-                name="Qty"
-              />
-            </div>
+            <Field
+              fieldName="Qty"
+              labelText="Qty."
+              validationText="This field is required, troesma."
+              inputType="number"
+              classes="five-fields-container__column-qty"
+            />
 
-            <div className="field ">
-              <label className="field__label" htmlFor="Price">
-                Price
-                <span className="field__validation">
-                  This field is required, troesma.
-                </span>
-              </label>
-              <input
-                className="field__input field__imput--type-number"
-                type="number"
-                name="Price"
-              />
-            </div>
+            <Field
+              fieldName="price"
+              labelText="Price"
+              validationText="This field is required, troesma."
+              inputType="number"
+              classes="five-fields-container__column-price"
+            />
 
-            <div className="field__total">
+            <div className="field__total five-fields-container__column-total">
               <h3 className="field__label">Total</h3>
               <div>0.00</div>
             </div>
 
-            <button className="field__button-delete field__button-delete--margin-top">
+            <button className="field__button-delete field__button-delete--separation-top">
               <img src={iconTrashCan} alt="Trash icon. Click to delete."></img>
             </button>
           </section>
 
           <section className="five-fields-container five-fields-container--hidden-labels">
-            <div className="field ">
-              <label className="field__label" htmlFor="ItemName">
-                Item Name
-                <span className="field__validation">
-                  This field is required, troesma.
-                </span>
-              </label>
-              <input className="field__input" type="text" name="ItemName" />
-            </div>
+            <Field
+              fieldName="itemName"
+              labelText="Item Name"
+              validationText="This field is required, troesma."
+              inputType="text"
+              classes="five-fields-container__column-item-name"
+            />
 
-            <div className="field ">
-              <label className="field__label" htmlFor="Qty">
-                Qty.
-                <span className="field__validation">
-                  This field is required, troesma.
-                </span>
-              </label>
-              <input
-                className="field__input field__imput--type-number field__input--center"
-                type="number"
-                name="Qty"
-                max="4"
-              />
-            </div>
+            <Field
+              fieldName="qty"
+              labelText="Qty."
+              validationText="This field is required, troesma."
+              inputType="number"
+              classes="five-fields-container__column-qty"
+            />
 
-            <div className="field ">
-              <label className="field__label" htmlFor="Price">
-                Price
-                <span className="field__validation">
-                  This field is required, troesma.
-                </span>
-              </label>
-              <input
-                className="field__input field__imput--type-number"
-                type="number"
-                name="Price"
-              />
-            </div>
+            <Field
+              fieldName="price"
+              labelText="Price"
+              validationText="This field is required, troesma."
+              inputType="number"
+              classes="five-fields-container__column-price"
+            />
 
-            <div className="field__total">
+            <div className="field__total five-fields-container__column-total">
               <h3 className="field__title--hidden">Total</h3>
               <div>0.00</div>
             </div>

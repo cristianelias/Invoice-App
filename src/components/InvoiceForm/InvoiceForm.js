0c6b9514cb2ad0f2509d926c5796b4a813ca1442
Cris Elias <cristian.elias@mercadolibre.com>
2022-01-30 05:49:39 -0300

    This is messy

--- src/components/InvoiceForm/InvoiceForm.js
+++ src/components/InvoiceForm/InvoiceForm.js
@@ -1,181 +1,299 @@
 // Dependencies
-// eslint-disable-next-line
-import { Formik, Form } from "formik";
-// eslint-disable-next-line
-import { createRoutesFromChildren, useNavigate } from "react-router-dom";
+import { Fragment } from "react";
+import { Formik, Form, FieldArray } from "formik";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
 
 // Components
-import Field from "./Field/Field";
+import InvoiceField from "./InvoiceField/InvoiceField";
 import Gradient from "../Gradient/Gradient";
 import PrimaryButton from "../Button/PrimaryButton/PrimaryButton";
 import SecondaryButton from "../Button/SecondaryButton/SecondaryButton";
 import TertiaryButton from "../Button/TertiaryButton/TertiaryButton";
-import FormRowList from "./FormRowList/FormRowList";
 
 // Styles
 import "./InvoiceForm.css";
 
+// Assets
+import iconTrashCan from "../../assets/icon-delete.svg";
+
+const inputDataByName = {
+  streetAddress: {
+    label: "Street Address",
+    classes: "",
+  },
+  city: {
+    label: "City",
+    classes: "",
+  },
+  postCode: {
+    label: "Post Code",
+    classes: "",
+  },
+  country: {
+    label: "Country",
+    classes: "",
+  },
+  clientName: {
+    label: "Client's Name",
+    classes: "",
+  },
+  clientEmail: {
+    label: "Client's Email",
+    classes: "",
+  },
+  invoiceDate: {
+    label: "Invoice Date",
+    classes: "",
+  },
+  paymentTerms: {
+    label: "Payment Terms",
+    classes: "",
+  },
+  projectDescription: {
+    label: "Project Description",
+    classes: "",
+  },
+  itemName: {
+    label: "Item Name",
+    classes: "",
+  },
+  qty: {
+    label: "Qty.",
+    classes: "",
+  },
+  price: {
+    label: "Price",
+    classes: "",
+  },
+  total: {
+    label: "Total",
+    classes: "",
+    // type: "calculated",
+  },
+};
+
+const chargesValues = {
+  itemName: "",
+  qty: "",
+  price: "",
+  total: "",
+};
+
+const formValues = {
+  from: {
+    streetAddress: "",
+    city: "",
+    postCode: "",
+    country: "",
+  },
+  to: {
+    clientName: "",
+    clientEmail: "",
+    streetAddress: "",
+    city: "",
+    postCode: "",
+    country: "",
+  },
+  details: {
+    invoiceDate: "",
+    paymentTerms: "",
+    projectDescription: "",
+  },
+  charges: [chargesValues],
+};
+
+const STRING_MIN_LENGTH = 4;
+const STRING_MAX_LENGTH = 30;
+const NUMBER_MIN_VALUE = 1;
+const PRICE_MAX_VALUE = 1000000;
+const QTY_MAX_VALUE = 1000;
+
+const commonSchemas = {
+  streetAddress: Yup.string()
+    .min(STRING_MIN_LENGTH)
+    .max(STRING_MAX_LENGTH)
+    .required("You need to specify a Street Address"),
+  city: Yup.string()
+    .min(STRING_MIN_LENGTH)
+    .max(STRING_MAX_LENGTH)
+    .required("You need to specify a City"),
+  postCode: Yup.string()
+    .min(STRING_MIN_LENGTH)
+    .max(STRING_MAX_LENGTH)
+    .required("You need to specify a Post Code"),
+  country: Yup.string()
+    .min(STRING_MIN_LENGTH)
+    .max(STRING_MAX_LENGTH)
+    .required("You need to specify a Country"),
+};
+
+const validationSchema = Yup.object().shape({
+  from: Yup.object().shape({
+    streetAddress: commonSchemas.streetAddress,
+    city: commonSchemas.city,
+    postCode: commonSchemas.postCode,
+    country: commonSchemas.country,
+  }),
+  to: Yup.object().shape({
+    clientName: Yup.string()
+      .min(STRING_MIN_LENGTH)
+      .max(STRING_MAX_LENGTH)
+      .required("You need to specify a Client Name"),
+    clientEmail: Yup.string()
+      .min(STRING_MIN_LENGTH)
+      .max(STRING_MAX_LENGTH)
+      .required("You need to specify an Client E-mail"),
+    streetAddress: commonSchemas.streetAddress,
+    city: commonSchemas.city,
+    postCode: commonSchemas.postCode,
+    country: commonSchemas.country,
+  }),
+  details: Yup.object().shape({
+    invoiceDate: Yup.string()
+      .min(STRING_MIN_LENGTH)
+      .max(STRING_MAX_LENGTH)
+      .required("You need to specify an Invoice Date"),
+    paymentTerms: Yup.string()
+      .min(STRING_MIN_LENGTH)
+      .max(STRING_MAX_LENGTH)
+      .required("You need to choose the desired Payment Terms"),
+    projectDescription: Yup.string()
+      .min(STRING_MIN_LENGTH)
+      .max(STRING_MAX_LENGTH)
+      .required("You need to specify a Project Description"),
+    itemList: Yup.array().of(
+      Yup.object().shape({
+        itemName: Yup.string()
+          .min(STRING_MIN_LENGTH)
+          .max(STRING_MAX_LENGTH)
+          .required("You need to specify an Item Name"),
+        quantity: Yup.number()
+          .min(NUMBER_MIN_VALUE)
+          .max(QTY_MAX_VALUE)
+          .required("You need to specify a Quantity")
+          .typeError("Quantity must be a number"),
+        price: Yup.number()
+          .min(NUMBER_MIN_VALUE)
+          .max(PRICE_MAX_VALUE)
+          .required("You need to specify a Price")
+          .typeError("Price must be a number"),
+      })
+    ),
+  }),
+});
+
 const InvoiceForm = (props) => {
-  const { title, footer } = props;
+  console.log("Exploded here");
+  const { title } = props;
   const navigate = useNavigate();
+  const submitHandler = (values) =>
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+    }, 500);
 
   return (
     <Formik
-      initialValues={{ fromStreetAddress: "" }}
-      // validate={(values) => {
-      //   const errors = {};
-      //   if (!values.email) {
-      //     errors.email = "Required";
-      //   } else if (
-      //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-      //   ) {
-      //     errors.email = "Invalid email address";
-      //   }
-      //   return errors;
-      // }}
-      onSubmit={(values, { setSubmitting }) => {
-        console.log(values);
-        console.log(setSubmitting);
-      }}
+      initialValues={formValues}
+      validationSchema={validationSchema}
+      onSubmit={submitHandler}
+      validateOnChange={true}
+      validateOnBlur={false}
     >
-      {({ submitForm, values, handleChange }) => (
+      {({ values }) => (
         <>
           <div className="invoice-form">
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+
             <Form className="invoice-form__form">
               {title}
 
-              <section className="three-input-container">
+              <fieldset>
                 <h2 className="h2">Bill From</h2>
 
-                <Field
-                  fieldName="fromStreetAddress"
-                  labelText="Street Address"
-                  validationText="This field is required, troesma."
-                  inputType="text"
-                  value={values.fromStreetAddress}
-                  onChange={handleChange}
-                />
+                {Object.keys(values.from).map((fieldName, index) => (
+                  <InvoiceField
+                    key={index}
+                    name={`from.${fieldName}`}
+                    text={inputDataByName[fieldName].label}
+                    classes={inputDataByName[fieldName].classes}
+                  />
+                ))}
+              </fieldset>
 
-                <Field
-                  fieldName="fromCity"
-                  labelText="City"
-                  validationText="This field is required, troesma."
-                  inputType="text"
-                  value={values.fromCity}
-                  onChange={handleChange}
-                />
-
-                <Field
-                  fieldName="fromPostCode"
-                  labelText="Post Code"
-                  validationText="This field is required, troesma."
-                  inputType="text"
-                  value={values.fromPostCode}
-                  onChange={handleChange}
-                />
-
-                <Field
-                  fieldName="fromCountry"
-                  labelText="Country"
-                  validationText="This field is required, troesma."
-                  inputType="text"
-                  value={values.fromCountry}
-                  onChange={handleChange}
-                />
-              </section>
-
-              <section>
+              <fieldset>
                 <h2 className="h2">Bill To</h2>
 
-                <Field
-                  fieldName="clientName"
-                  labelText="Client's Name"
-                  validationText="This field is required, troesma."
-                  inputType="text"
-                  value={values.clientName}
-                  onChange={handleChange}
-                />
-
-                <Field
-                  fieldName="clientEmail"
-                  labelText="Client's Email"
-                  validationText="This field is required, troesma."
-                  inputType="email"
-                  value={values.clientEmail}
-                  onChange={handleChange}
-                />
-
-                <Field
-                  fieldName="toStreetAddress"
-                  labelText="Street Address"
-                  validationText="This field is required, troesma."
-                  inputType="text"
-                  value={values.toStreetAddress}
-                  onChange={handleChange}
-                />
-
-                <section className="three-input-container">
-                  <Field
-                    fieldName="toCity"
-                    labelText="City"
-                    validationText="This field is required, troesma."
-                    inputType="text"
-                    value={values.toCity}
-                    onChange={handleChange}
+                {Object.keys(values.to).map((fieldName, index) => (
+                  <InvoiceField
+                    key={index}
+                    name={`to.${fieldName}`}
+                    text={inputDataByName[fieldName].label}
+                    classes={inputDataByName[fieldName].classes}
                   />
+                ))}
+              </fieldset>
 
-                  <Field
-                    fieldName="toPostCode"
-                    labelText="Post Code"
-                    validationText="This field is required, troesma."
-                    inputType="text"
-                    value={values.toPostCode}
-                    onChange={handleChange}
+              <fieldset>
+                {Object.keys(values.details).map((fieldName, index) => (
+                  <InvoiceField
+                    key={index}
+                    name={`details.${fieldName}`}
+                    text={inputDataByName[fieldName].label}
+                    classes={inputDataByName[fieldName].classes}
                   />
+                ))}
+              </fieldset>
 
-                  <Field
-                    fieldName="toCountry"
-                    labelText="Country"
-                    validationText="This field is required, troesma."
-                    inputType="text"
-                    value={values.toCountry}
-                    onChange={handleChange}
-                  />
-                </section>
-              </section>
+              <fieldset>
+                <FieldArray name="charges">
+                  {({ push, remove }) => (
+                    <div>
+                      {values.charges.map((item, index) => (
+                        <div key={index}>
+                          {Object.keys(item).map((fieldName) => {
+                            const fieldMeta = inputDataByName[fieldName];
 
-              <section className="two-input-container">
-                <Field
-                  fieldName="invoiceDate"
-                  labelText="Invoice Date"
-                  validationText="This field is required, troesma."
-                  inputType="date"
-                  placeholder="21 Aug 2021"
-                  value={values.invoiceDate}
-                  onChange={handleChange}
-                />
+                            return (
+                              <Fragment key={`${index}x${fieldName}`}>
+                                <InvoiceField
+                                  name={`charges[${index}].${fieldName}`}
+                                  text={fieldMeta.label}
+                                  classes={fieldMeta.classes}
+                                  // type={fieldMeta.type}
+                                />
+                              </Fragment>
+                            );
+                          })}
 
-                <Field
-                  fieldName="paymentTerms"
-                  labelText="Payment Terms"
-                  validationText="This field is required, troesma."
-                  inputType="text"
-                  value={values.paymentTerms}
-                  onChange={handleChange}
-                />
-
-                <Field
-                  fieldName="projectDescription"
-                  labelText="Project Description"
-                  validationText="This field is required, troesma."
-                  inputType="text"
-                  placeholder="e.g. Graphic Design Service"
-                  value={values.projectDescription}
-                  onChange={handleChange}
-                />
-              </section>
-
-              <FormRowList />
+                          <div>
+                            <h3>Total</h3>
+                            <div>
+                              {parseInt(item.price) * parseInt(item.qty)}
+                            </div>
+                          </div>
+                          <button
+                            onClick={() => {
+                              remove(index);
+                            }}
+                          >
+                            <img
+                              src={iconTrashCan}
+                              alt="Trash icon. Click to delete."
+                            ></img>
+                          </button>
+                        </div>
+                      ))}
+                      <button
+                        className="five-fields-container__add-new-item button-component tertiary-button"
+                        onClick={() => push(chargesValues)}
+                      >
+                        + Add New Item
+                      </button>
+                    </div>
+                  )}
+                </FieldArray>
+              </fieldset>
 
               <p className="field__validation five-fields-container__validation">
                 - All fields must be added
@@ -194,7 +312,7 @@ const InvoiceForm = (props) => {
               text="Save as Draft"
             />
 
-            <PrimaryButton onClick={submitForm} text="Save & Send" />
+            <PrimaryButton onClick={submitHandler} text="Save & Send" />
           </footer>
         </>
       )}

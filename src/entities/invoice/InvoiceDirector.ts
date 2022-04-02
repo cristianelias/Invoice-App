17011fd7ae3ad0acf3daee20466cd71638358a4a
Cris Elias <cristian.elias@mercadolibre.com>
2022-04-02 02:24:44 -0300

    Bussiness logic change: when editing a Draft, it should be saved as a Pending invoice. When saving a Paid, should remain as Paid.

--- src/entities/invoice/InvoiceDirector.ts
+++ src/entities/invoice/InvoiceDirector.ts
@@ -1,22 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import DraftInvoiceBuilder from "./DraftInvoiceBuilder";
 import EditedInvoiceBuilder from "./EditedInvoiceBuilder";
 import Invoice from "./Invoice";
 import InvoiceBuilder from "./InvoiceBuilder";
 
 class InvoiceDirector {
-  buildNewInvoice(values): Invoice {
+  buildNewInvoice(values: object): Invoice {
     return this.build(new InvoiceBuilder(), values);
   }
 
-  buildDraftInvoice(values): Invoice {
+  buildDraftInvoice(values: object): Invoice {
     return this.build(new DraftInvoiceBuilder(), values);
   }
 
-  buildEditedInvoice(id, status, values): Invoice {
-    return this.build(new EditedInvoiceBuilder(id, status), values);
+  buildEditedInvoice(id: string, status: string, values: object): Invoice {
+    const invoiceStatus = status === "draft" ? "pending" : status;
+    return this.build(new EditedInvoiceBuilder(id, invoiceStatus), values);
   }
 
-  private build(builder: InvoiceBuilder, values): Invoice {
+  private build(builder: InvoiceBuilder, values: any): Invoice {
     const { from, to, details, charges } = values;
 
     return builder
